93a3dcf21a11a576866335b8ae25638e517156fe
CHARLIER Martin <m.charlier@students.ephec.be>
2024-04-24 13:32:58 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from "dotenv";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: process.env.REACT_APP_API_URL,
         changeOrigin: true
       }
     }
